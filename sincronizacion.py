94a8a030fc7bc40dbee48a27cf026783748beea3
Joel0347 <127158086+Joel0347@users.noreply.github.com>
2024-12-04 19:09:10 -0500

    captar la ruta de la carpeta de sincronizacion

--- sincronizacion.py
+++ sincronizacion.py
@@ -1,8 +1,13 @@
 import os
+import sys
 
 def main():
+    # El usuario debe escribir: python sincronizacion.py <ruta_de_la_carpeta>
+    if len(sys.argv) != 2:
+        print("Uso: python sync_service.py <ruta_de_la_carpeta>")
+        sys.exit(1)
 
-    sync_folder = "" # aqui pondremos la direccion de la carpeta para sincronizar
+    sync_folder = sys.argv[1]
 
     # Verifica si la carpeta especificada no existe.
     if not os.path.exists(sync_folder):
