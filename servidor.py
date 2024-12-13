798b45435f799a35e798497b387472c85144c3ae
Joel0347 <127158086+Joel0347@users.noreply.github.com>
2024-12-13 09:52:43 -0500

    Añadida configuración del servidor y hecha funcional la sincronización

--- servidor.py
+++ servidor.py
@@ -1,21 +1,35 @@
+import socket
+import threading
 import os
 
 # Dirección y puerto del servidor
 local_addr = "B4:8C:9D:D4:8E:BA"
 port = 30
 
-#iniciar servidor
+
 def start_server(local_addr, port, sync_folder):
+    sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+    sock.bind((local_addr, port))
+    sock.listen(1)
+
     print(f"Servidor escuchando en {local_addr}:{port}")
 
     while True:
         try:
-            filename = "hello_world"
-            address = [local_addr]
+            client_sock, address = sock.accept()
+            data = client_sock.recv(1024).decode()
+            filename, filedata = data.split('::')
+
+            with open(os.path.join(sync_folder, filename), 'w') as f:
+                f.write(filedata)
+
             print(f"Archivo recibido: {filename} de {address[0]}")
+            client_sock.close()
         except Exception as e:
             print(f"Error al recibir archivo: {e}")
 
-#iniciar servicio de sincronización
+
 def start_sync_service(sync_folder):
-    print("")
+    server_thread = threading.Thread(target=start_server, args=(local_addr, port, sync_folder))
+    server_thread.daemon = True
+    server_thread.start()
--- sincronizacion.py
+++ sincronizacion.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from servidor import start_sync_service
+from cliente import watch_folder
 
 def main():
     # El usuario debe escribir: python sincronizacion.py <ruta_de_la_carpeta>
@@ -15,6 +16,7 @@ def main():
         os.makedirs(sync_folder)  # Crea la carpeta especificada.
 
     start_sync_service(sync_folder)
+    watch_folder(sync_folder)
 
 if __name__ == "__main__":
     main()
